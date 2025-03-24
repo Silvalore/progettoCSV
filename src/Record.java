deb0f18a2269c8d80455e2339d24fc152b571691
silva.21140 <silva.lorenzo.studente@itispaleocapa.it>
2025-03-24 12:45:27 +0100

    s

---
+++ src/Record.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+    public class Record {
+        private String[] campi;
+        private int mioValore;
+        private boolean cancellato;
+
+        public Record(String[] campi) {
+            this.campi = campi;
+            this.mioValore = new Random().nextInt(11) + 10; // Genera valore tra 10 e 20
+            this.cancellato = false;
+        }
+
+        public String[] getCampi() {
+            return campi;
+        }
+
+        public int getMioValore() {
+            return mioValore;
+        }
+
+        public boolean isCancellato() {
+            return cancellato;
+        }
+
+        public void setCancellato(boolean cancellato) {
+            this.cancellato = cancellato;
+        }
+
+        @Override
+        public String toString() {
+            return String.join(",", campi) + "," + mioValore + "," + (cancellato ? "1" : "0");
+        }
+    }
